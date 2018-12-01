99d3a97ca358b63854de8e2eec455124e1d63ea5
Xianda Gao <gaoxiandathss@gmail.com>
2018-12-01 14:48:47 -0500

    select model

---
+++ select_model.py
@@ -0,0 +1,69 @@
+import numpy as np
+import itertools
+
+from scipy import linalg
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+from sklearn import mixture
+
+from features import extract_features_from_file
+import constants
+
+
+print(__doc__)
+
+# Number of samples per component
+n_samples = 500
+
+# Generate random sample, two components
+X = extract_features_from_file(constants.log_path, constants.features)
+
+lowest_bic = np.infty
+bic = []
+n_components_range = range(1, 10)
+cv_types = ['spherical', 'tied', 'diag', 'full']
+for cv_type in cv_types:
+    for n_components in n_components_range:
+        # Fit a Gaussian mixture with EM
+        gmm = mixture.GaussianMixture(n_components=n_components,
+                                      covariance_type=cv_type)
+        gmm.fit(X)
+        bic.append(gmm.bic(X))
+        if bic[-1] < lowest_bic:
+            lowest_bic = bic[-1]
+            best_gmm = gmm
+            best_type = cv_type
+            best_n_com = n_components
+
+print(lowest_bic)
+
+bic = np.array(bic)
+color_iter = itertools.cycle(['navy', 'turquoise', 'cornflowerblue',
+                              'darkorange'])
+clf = best_gmm
+bars = []
+
+# Plot the BIC scores
+plt.figure(figsize=(8, 6))
+spl = plt.subplot(2, 1, 1)
+for i, (cv_type, color) in enumerate(zip(cv_types, color_iter)):
+    xpos = np.array(n_components_range) + .2 * (i - 2)
+    bars.append(plt.bar(xpos, bic[i * len(n_components_range):
+                                  (i + 1) * len(n_components_range)],
+                        width=.2, color=color))
+plt.xticks(n_components_range)
+plt.ylim([bic.min() * 1.01 - .01 * bic.max(), bic.max()])
+plt.title('BIC score per model')
+xpos = np.mod(bic.argmin(), len(n_components_range)) + .65 +\
+    .2 * np.floor(bic.argmin() / len(n_components_range))
+plt.text(xpos, bic.min() * 0.97 + .03 * bic.max(), '*', fontsize=14)
+spl.set_xlabel('Number of components')
+spl.legend([b[0] for b in bars], cv_types)
+
+
+plt.xticks(())
+plt.yticks(())
+plt.title('Selected GMM: {} model, {} components'.format(best_type, best_n_com))
+plt.subplots_adjust(hspace=.35, bottom=.02)
+plt.show()
